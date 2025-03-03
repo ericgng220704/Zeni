1fe69d685542340f4ef4a06f5aa39f863a8b4863
gnguyen2 <gnguyen2@academic.rrc.ca>
2025-03-03 11:18:09 -0600

    fix sleep time

--- app/api/workflows/transactions/recurring/route.ts
+++ app/api/workflows/transactions/recurring/route.ts
@@ -84,7 +84,7 @@ export const { POST } = serve(async (context) => {
 
     // 3. Sleep until the next poll.
     // Adjust the polling frequency (here, 24 hours) as necessary.
-    const pollIntervalMs = 24 * 60 * 60 * 1000; // 24 hours
+    const pollIntervalMs = 24 * 60 * 60; // 24 hours
     console.log(
       `Sleeping for ${
         pollIntervalMs / (1000 * 60 * 60)
